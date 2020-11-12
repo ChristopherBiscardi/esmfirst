d5a3237e012e7a04fccf3a0f3560b152f5defcdb
Christopher Biscardi <chris@christopherbiscardi.com>
2020-11-12 10:03:40 -0800

    opengraph

--- src/page-wrapper.js
+++ src/page-wrapper.js
@@ -15,6 +15,26 @@ export default function PageWrapper(props) {
     <MDXProvider components={components}>
       <div>
         <Helmet>
+          <meta charSet="utf-8" />
+          <meta name="viewport" content="width=device-width, initial-scale=1" />
+
+          <title>Rust Adventurers!</title>
+          <meta name="twitter:title" content="Rust Adventurers" />
+          <meta name="og:title" content="Rust Adventurers" />
+          <meta name="description" content="Start your Rust Adventure!" />
+          <meta
+            name="twitter:description"
+            content="Start your Rust Adventure!"
+          />
+          <meta name="og:type" content="website" />
+          <meta name="twitter:site" content="@chrisbiscardi" />
+          <meta name="twitter:creator" content="@chrisbiscardi" />
+          <meta name="twitter:card" content="summary_large_image" />
+
+          <meta
+            name="twitter:image"
+            content={`https://www.rustadventure.rs/opengraph-default.png`}
+          />
           <link rel="stylesheet" href="/styles.css" />
         </Helmet>
         {props.children}
